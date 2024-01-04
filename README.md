2d311b34c1390937f601055342aaad5192d9a0d4
asiek728 <asiek728@gmail.com>
2024-01-04 21:42:33 +0000

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-pipenv install
-pipenv shell
+## Installation
+- Copy the repository
+- Create .env file with following variables:
+    - FLASK_DEBUG=1
+    - SQLALCHEMY_DATABASE_URI="postgresql://......"
 
-python seed.py
-python app.py
+- Run the following commands:
+    - `pipenv install`
+    - `pipenv shell`
+    - `python seed.py`
 
+## Running
+- Run the following command:
+- `python app.py`
+- The system should run on http://localhost:4000/
 
-pipenv run test
-pipenv run coverage
+## Testing
+- Run the following commands:
+    - `pipenv run test`
+    - `pipenv run coverage`
+
+## Available endpoints
+- "GET /",
+- "GET /authors",
+- "GET /authors/<int:id>",
+- "POST /authors",
+- "PATCH /authors/<int:id>",
+- "DELETE /authors/<int:id>",
+- "GET /books",
+- "GET /books/<int:id>",
+- "POST /books",
+- "PATCH /books/<int:id>",
+- "DELETE /books/<int:id>"
--- test_project.py
+++ test_project.py
@@ -1,12 +1,38 @@
-# import pytest
-# import application.stuff as stuff
-# import sys
-# from unittest import mock
-# #from application import app ## app from __init__.py
+# from flask import Flask, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_testing import TestCase
+# from application import app, db, create_app
 
-# @pytest.mark.parametrize('x, y, expected', [(2, 4, 6), (2, 2, 4), (2, 3, 5)])
-# def test_do_maths(x, y, expected):
-#     assert stuff.do_maths(x, y) == expected
+# def create_test_app():
+#     app = Flask(__name__)
+#     app.config['TESTING'] = True
+#     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://gskxgcwq:QwiVyTkjieyQ0l0X19Uv--oQnLF58Tjb@tyke.db.elephantsql.com/gskxgcwq"
+#     # Dynamically bind SQLAlchemy to application
+#     db.init_app(app)
+#     app.app_context().push() # this does the binding
+#     return app
 
 
-# #pipenv run coverage
+# class MyTest(TestCase):
+
+#     # I removed some config passing here
+#     def create_app(self):
+#         return create_test_app()
+
+#     def setUp(self):
+#         db.create_all()
+
+#     def tearDown(self):
+
+#         db.session.remove()
+#         db.drop_all()
+
+
+# @app.route("/")
+# def some_json():
+#     return jsonify(success=True)
+
+# class TestViews(TestCase):
+#     def test_some_json(self):
+#         response = self.client.get("/")
+#         self.assertEquals(response.json, dict(success=True))
