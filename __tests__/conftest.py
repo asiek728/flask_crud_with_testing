c014e3b807a618e7e9899004d0adc9234581dfe9
asiek728 <asiek728@gmail.com>
2024-01-04 17:28:48 +0000

    add cache to gitignore

--- __tests__/conftest.py
+++ __tests__/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from application.authors.model import Author, Book
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def create_author():
     author = Author('Britney', 20, 'fantasy')
     return author
 
-@pytest.fixture(scope='module')
+@pytest.fixture()
 def create_book():
     book = Book('Harry Potter', 'fantasy', 1)
     return book
