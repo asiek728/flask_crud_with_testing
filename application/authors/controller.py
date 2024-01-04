cd84460561cc82b4855bb1ca09fc90661c0a44f1
asiek728 <asiek728@gmail.com>
2024-01-04 10:12:07 +0000

    add GET, PATCH and DELETE for authors/id

--- application/authors/controller.py
+++ application/authors/controller.py
@@ -10,7 +10,13 @@ def index():
         return jsonify({ "data": [c.json for c in authors] }), 200
     except:
         raise exceptions.InternalServerError(f"Something went wrong")
-
+    
+def show(id):
+    character = Author.query.filter_by(id=id).first()
+    try:
+        return jsonify({ "data": character.json }), 200
+    except:
+        raise exceptions.NotFound(f"Author not found")
 
 def create():
     try:
@@ -24,3 +30,20 @@ def create():
         return jsonify({ "data": new_author.json }), 201
     except:
         raise exceptions.BadRequest(f"We couldn't add this author")
+
+def update(id):
+    data = request.json
+    author = Author.query.filter_by(id=id).first()
+
+    for (attribute, value) in data.items():
+        if hasattr(author, attribute):
+            setattr(author, attribute, value)
+
+    db.session.commit()
+    return jsonify({ "data": author.json })
+
+def destroy(id):
+    author = Author.query.filter_by(id=id).first()
+    db.session.delete(author)
+    db.session.commit()
+    return "Author has been deleted", 204
--- application/authors/routes.py
+++ application/authors/routes.py
@@ -1,9 +1,31 @@
 from flask import request
 from werkzeug import exceptions
 from application import app # app from __init__.
-from .controller import index, create#, show, update, destroy
+from .controller import index, create, show, update, destroy
 
 @app.route('/authors', methods=["GET", "POST"])
 def handle_authors():
     if request.method == "POST": return create()
     if request.method == "GET": return index()
+
+
+@app.route('/authors/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def handle_author(id):
+    if request.method == "GET": return show(id)
+    if request.method == "PATCH": return update(id)
+    if request.method == "DELETE": return destroy(id)
+
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return { "error": f"Error {err}"}, 404
+
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return { "error": f"Error {err} "}, 500
+
+
+@app.errorhandler(exceptions.BadRequest)
+def handle_400(err):
+    return { "error": f"Error {err}" }, 400
